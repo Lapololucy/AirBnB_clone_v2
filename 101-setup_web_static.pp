4591d5f75813d09676ecc97bbfcda70bd5d84fca
Lapololucy <lapololucy@gmail.com>
2023-05-07 15:41:45 +0000

    redoing task 0

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,29 +1,91 @@
-# Install and config Ngnix
-exec { 'update':
-  command => '/usr/bin/env sudo apt-get -y update',
-}
--> exec {'instNginx':
-  command => '/usr/bin/env sudo apt-get -y install nginx',
-}
--> exec {'crea1':
-  command => '/usr/bin/env sudo mkdir -p /data/web_static/releases/test/',
-}
--> exec {'crea2':
-  command => '/usr/bin/env sudo mkdir -p /data/web_static/shared/',
-}
--> exec {'fake':
-  command => '/usr/bin/env sudo echo "html fake" > /data/web_static/releases/test/index.html',
-}
--> exec {'simlink':
-  command => '/usr/bin/env sudo ln -sf /data/web_static/releases/test /data/web_static/current',
-}
--> exec {'chown':
-  command => '/usr/bin/env sudo chown -R ubuntu:ubuntu /data',
-}
--> exec {'configure':
-  command => '/usr/bin/env sudo sed -i "/listen 80 default_server/a location /hbnb_static/ { alias /data/web_static/current/;}" /etc/nginx/sites-available/default',
-}
--> exec {'restart':
-  command => '/usr/bin/env sudo service nginx restart',
+class web_static {
+  # Install nginx if it's not already installed
+  package { 'nginx':
+    ensure => installed,
+  }
+
+  # Create necessary directories
+  file { '/data':
+    ensure => directory,
+  }
+
+  file { '/data/web_static':
+    ensure => directory,
+    owner  => 'ubuntu',
+    group  => 'ubuntu',
+    mode   => '0775',
+  }
+
+  file { '/data/web_static/releases':
+    ensure => directory,
+    owner  => 'ubuntu',
+    group  => 'ubuntu',
+    mode   => '0775',
+  }
+
+  file { '/data/web_static/shared':
+    ensure => directory,
+    owner  => 'ubuntu',
+    group  => 'ubuntu',
+    mode   => '0775',
+  }
+
+  file { '/data/web_static/releases/test':
+    ensure => directory,
+    owner  => 'ubuntu',
+    group  => 'ubuntu',
+    mode   => '0775',
+  }
+
+  # Create index.html file
+  file { '/data/web_static/releases/test/index.html':
+    ensure  => file,
+    owner   => 'ubuntu',
+    group   => 'ubuntu',
+    mode    => '0644',
+    content => '<html>
+  <head>
+  </head>
+  <body>
+    ALX School
+  </body>
+</html>',
+  }
+
+  # Create symbolic link
+  file { '/data/web_static/current':
+    ensure => 'link',
+    target => '/data/web_static/releases/test',
+    owner  => 'ubuntu',
+    group  => 'ubuntu',
+    mode   => '0775',
+    require => File['/data/web_static/releases/test'],
+  }
+
+  # Set up nginx configuration
+  file { '/etc/nginx/sites-available/default':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => 'server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+        root /data/web_static/releases/test;
+        index index.html;
+        server_name _;
+        location /hbnb_static/ {
+            alias /data/web_static/current/;
+            autoindex off;
+        }
+    }',
+    notify => Service['nginx'],
+  }
+
+  # Start/restart nginx service
+  service { 'nginx':
+    ensure => running,
+    enable => true,
+  }
 }
 
